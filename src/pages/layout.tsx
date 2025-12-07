31799fa95556f512458b4eab51cbbd5169094fa5
Israel de Souza <israellimas@hotmail.com>
2025-12-06 23:20:53 -0300

    chore: configuração inicial

---
+++ src/pages/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const fontGeistSans = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist",
+});
+
+const fontGeistMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-geist-mono",
+  weight: "400",
+});
+
+export const metadata: Metadata = {
+  title: "Buscapet",
+  description: "A plataforma definitiva para encontrar o pet ideal para você.",
+  icons: {
+    icon: "/logo.png",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={`${fontGeistSans.variable} ${fontGeistMono.variable} font-sans antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
