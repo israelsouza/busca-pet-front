71a4082914ebe65391f7155d9b05c615b04c78cd
Israel de Souza <israellimas@hotmail.com>
2025-12-30 19:50:58 -0300

    feat: add `Hero` section in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "./_components/Header";
+import Hero from "./_components/Hero";
 
 export default function EmConstrucao() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 }
